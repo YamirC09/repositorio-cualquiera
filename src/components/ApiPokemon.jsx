c162faf878f59ce86910e140fa99e3bc5311fa81
YamirC09 <yamirc09@gmail.com>
2023-06-03 23:16:10 -0500

    Inicializamos variables

--- src/components/ApiPokemon.jsx
+++ src/components/ApiPokemon.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const ApiPokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const [paginacion, setPaginacion] = useState(1);
+    const [mostrarPokemon, setMostrarPokemon] = useState(false);
+    const [pokemonDetalles, setPokemonDetalles] = useState([]);
+
   return (
     <div>ApiPokemon</div>
   )
