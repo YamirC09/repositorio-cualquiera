5c62ce9683f4aad8706b54a80842d43d41e535e0
YamirC09 <yamirc09@gmail.com>
2023-06-03 23:18:10 -0500

    Metodo para traer pokemon y sus detalles

--- src/components/ApiPokemon.jsx
+++ src/components/ApiPokemon.jsx
@@ -6,6 +6,34 @@ const ApiPokemon = () => {
     const [mostrarPokemon, setMostrarPokemon] = useState(false);
     const [pokemonDetalles, setPokemonDetalles] = useState([]);
 
+    useEffect(() => {
+        if (mostrarPokemon) {
+          traerPokemon();
+        }
+      }, [paginacion, mostrarPokemon]);
+    
+      const traerPokemon = async () => {
+        try {
+          const res = await fetch(
+            `https://pokeapi.co/api/v2/pokemon?offset=${(paginacion - 1) * 20}&limit=20`
+          );
+          const respuesta = await res.json();
+          const auxPokemon = respuesta.results;
+          setPokemon(auxPokemon);
+    
+          const detallesPromises = auxPokemon.map(async (aux) => {
+            const res = await fetch(aux.url);
+            const respuesta = await res.json();
+            return respuesta;
+          });
+    
+          const detallesPokemon = await Promise.all(detallesPromises);
+          setPokemonDetalles(detallesPokemon);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
   return (
     <div>ApiPokemon</div>
   )
